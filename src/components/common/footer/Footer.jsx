63a41bb6300659250dffbc44ad96c3601e0794f4
Jose Terrazas <jozaguts@gmail.com>
2026-05-09 01:30:31 -0600

    Enhance footer design and update logo; add background styling and new color variables

--- src/components/common/footer/Footer.jsx
+++ src/components/common/footer/Footer.jsx
@@ -17,7 +17,9 @@ const Footer = () => {
     <div className="pt-25 md:pt-40 content max-2xl:px-3">
       <div className="flex max-md:flex-col justify-between mx-0 items-center h-full w-full text-neutral-200">
         <a href="#" className="flex items-center border-0">
-          <img src={logo} className="h-8 sm:h-14 rounded-2xl" alt="logo" />
+         <div className={'bg-white/80 rounded-2xl p-1.5'}>
+           <img src={logo} className="h-8 sm:h-14 rounded-2xl" alt="logo" />
+         </div>
           <p className="text-3xl sm:text-[32px] my-auto ms-[12px] font-semibold">
             LambertWorks
           </p>
